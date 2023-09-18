bc42ab63e03f536ca8bbab214f1be4dcb8aa24b6
nelvickus <145201653+nelvickus@users.noreply.github.com>
2023-09-18 15:02:29 +0300

    Add files via upload

---
+++ tulpan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	puts("      |\\  /\\  /|        ");
+	puts("      | \\/  \\/ |");
+	puts("      |        |");
+	puts("       \\______/");
+	puts("          \\_/     ");
+	puts("        __ |   ");
+	puts("       |  \\| ");
+	puts("        \\__| ");
+	puts("           |  ");
+}
